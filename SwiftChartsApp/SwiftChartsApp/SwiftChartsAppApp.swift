a931407376df95edeeafa40db6b31fbf82767683
Manas Vijaywargiya <manasdevslopes@gmail.com>
2024-12-16 17:43:31 +0530

    first commit

---
+++ SwiftChartsApp/SwiftChartsApp/SwiftChartsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsAppApp.swift
+//  SwiftChartsApp
+//
+//  Created by MANAS VIJAYWARGIYA on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
